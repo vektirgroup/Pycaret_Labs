e4fbb0a69f91821e463241bd598d2265a43b5fe1
vektirgroup <vektirgroup@gmail.com>
2021-11-20 14:20:04 -0600

    basic streamlit app

---
+++ Lab_1/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Sidebar section ==============
+sb_title = st.sidebar.title('Vektir Labs - Pycaret')
+
+# Main page section ============
+st.title('Pycaret Labs - Setup')
+st.subheader('Vektir Labs brought to you by The Vektir Group, llc')
+
