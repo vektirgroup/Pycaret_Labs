2a8351c05518fe49ec3cf34befb83f10c1fc58a9
Tyler  Hunt <Ty@Tylers-MBP2.nxlink.com>
2021-11-23 06:55:31 -0600

    Add wide layout

--- Lab_1 - Setup/app.py
+++ Lab_1 - Setup/app.py
@@ -2,7 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
+# Page Config ==============
+st.set_page_config(
+    page_title=None, 
+    page_icon=None, 
+    layout="wide", 
+    initial_sidebar_state="auto", 
+    menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     })
 
 # Sidebar section ==============
 st.sidebar.title('Vektir Labs')
@@ -20,23 +30,21 @@ with st.sidebar.expander('Reference Links'):
 # Main image ============
 st.image('../assets/PyCaret_Header.png',caption='pycaret.org')
 
-# Main page section ============
+# Main page ============
 st.header('Pycaret - Lab #1')
+
+# Intro ============
 st.subheader('What is Pycaret')
-
 st.write('''Well, according to the the official Pycaret Gihub account.....''')
-
 st.write('''PyCaret is an open-source, low-code machine learning library in Python that 
          automates machine learning workflows. It is an end-to-end machine learning 
          and model management tool that speeds up the experiment cycle exponentially 
          and makes you more productive.''')
-
 st.write('''Pycaret was created by @Moez Ali''')
-
-# st.write('''''')
-st.subheader('''Project Setup''')
+st.write('')
 
 # Install Python 3 
+st.subheader('''Project Setup''')
 st.markdown('''
          I am going to assume that you have not installed the necessary tools install yet, so I will 
          share the tools/processes I use to get up and running as quickly as possible!
@@ -55,6 +63,7 @@ st.image('../assets/Terminal.png',caption="Python 3.x installed correctly")
 st.markdown('''**Note:** If you receive an error message in the terminal or CMD prompt
             when running 'python3 --version' then consult the python install guide 
             [here](https://www.python.org/search/?q=install&submit=)''')
+
 
 # Install VS Code
 st.markdown('''
